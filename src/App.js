412c3c8e89809e2589b25a32213866ad73354ee8
Javlon <59303287+javlona@users.noreply.github.com>
2022-06-21 20:31:17 -0500

    tailwind

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="max-w-sm rounded shadow-lg">
+        <img src='https://source.unsplash.com/random' className='w-full'/>
+        <div className='px-6 py-4'>
+          <div className='font-bold text-black text-xl mb-2'>
+            Photo by Picasso
+          </div>
+          <ul>
+            <li>
+              <strong>Views:</strong>
+              4000
+            </li>
+            <li>
+              <strong>Downloads:</strong>
+              4000
+            </li>
+            <li>
+              <strong>Likes:</strong>
+              4000
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
